f55e5aab796cccf27ef5d8364225cf83e7340f77
Dan Souza <Souza.music@outlook.com>
2022-03-01 12:01:55 -0800

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your code here
+function submitData (name, email) {
+    
+    const configurationObject = {
+        method: "POST",
+        headers: {
+         "Content-Type": "application/json",
+         Accept: "application/json",
+        },
+        body:JSON.stringify({
+            name, email
+        })
+    }
+ return fetch("http://localhost:3000/users", configurationObject)
+    .then(resp => resp.json())
+    .then(data => document.body.append(data.id)) 
+    .catch(error => document.body.innerHTML = error.message);
+};
+
